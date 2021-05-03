d248c2eaec774db41b4d70f3c67b90d00476d102
julianpebe <julianperezbadon@hotmail.com>
2021-05-03 20:06:25 -0300

    Crea Mascota, TipoMascota, Caracteristica y TipoCaracteristica

---
+++ src/main/java/constants/TipoMascota.java
@@ -0,0 +1,6 @@
+package constants;
+
+public enum TipoMascota {
+  PERRO,
+  GATO,
+}
---
+++ src/main/java/domain/mascota/TipoCaracteristica.java
@@ -0,0 +1,17 @@
+package domain.mascota;
+
+import static constants.Mensajes.NOT_NULO;
+
+import java.util.Objects;
+
+public class TipoCaracteristica {
+  private final String descripcion;
+
+  public TipoCaracteristica(String descripcion) {
+    this.descripcion = Objects.requireNonNull(descripcion, NOT_NULO.mensaje("descripcion"));
+  }
+
+  public String getDescripcion() {
+    return descripcion;
+  }
+}
