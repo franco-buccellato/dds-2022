b61a1263308f190e8fabb39d7659f2f44ca1f511
Juan Reggiardo <juanreggiardo@aol.com>
2021-05-07 00:33:39 -0300

    Foto de mascota en repositorio

--- src/test/java/domain/RepositorioMascotasEncontradasTest.java
+++ src/test/java/domain/RepositorioMascotasEncontradasTest.java
@@ -7,8 +7,14 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -27,18 +33,20 @@ public class RepositorioMascotasEncontradasTest {
   private LocalDate fechaSistemaTest = LocalDate.of (2021, 5, 5);
 
   @BeforeEach
-  public void inicio(){
+  public void inicio() throws IOException {
+    Image fotoPerro1Encontrado = ImageIO.read(new File("resources/images/perroEncontrado1.jpg"));
+    List<Image> fotos = new ArrayList<>(Collections.singletonList(fotoPerro1Encontrado));
     fechaSistema = Constantes.getConstates().getFechaSistema();
     Constantes.getConstates().setFechaSistema (fechaSistemaTest);
 
-    pastorAleman = new MascotaEncontrada(null, "perro pastorAleman" , "", LocalDate.of(2021, 5, 4));
-    bulldog		 = new MascotaEncontrada(null, "perro bulldog"		, "", LocalDate.of(2021, 5, 3));
-    husky		 = new MascotaEncontrada(null, "perro husky"		, "", LocalDate.of(2021, 5, 2));
-    poodle		 = new MascotaEncontrada(null, "perro poodle"		, "", LocalDate.of(2021, 5, 1));
-    persa		 = new MascotaEncontrada(null, "gato persa"			, "", LocalDate.of(2021, 4, 12));
-    maineCoon	 = new MascotaEncontrada(null, "gato maineCoon"		, "", LocalDate.of(2021, 3, 15));
-    siames		 = new MascotaEncontrada(null, "gato siames"		, "", LocalDate.of(2021, 3, 15));
-    bengala		 = new MascotaEncontrada(null, "gato bengala"		, "", LocalDate.of(2021, 2, 10));
+    pastorAleman = new MascotaEncontrada(fotos, "perro pastorAleman" , "", LocalDate.of(2021, 5, 4));
+    bulldog		 = new MascotaEncontrada(fotos, "perro bulldog"		, "", LocalDate.of(2021, 5, 3));
+    husky		 = new MascotaEncontrada(fotos, "perro husky"		, "", LocalDate.of(2021, 5, 2));
+    poodle		 = new MascotaEncontrada(fotos, "perro poodle"		, "", LocalDate.of(2021, 5, 1));
+    persa		 = new MascotaEncontrada(fotos, "gato persa"			, "", LocalDate.of(2021, 4, 12));
+    maineCoon	 = new MascotaEncontrada(fotos, "gato maineCoon"		, "", LocalDate.of(2021, 3, 15));
+    siames		 = new MascotaEncontrada(fotos, "gato siames"		, "", LocalDate.of(2021, 3, 15));
+    bengala		 = new MascotaEncontrada(fotos, "gato bengala"		, "", LocalDate.of(2021, 2, 10));
 
     repoTest = RepositorioMascotaEncontrada.getRepositorio();
     repoTest.setMascotasEncontradas(Arrays.asList(pastorAleman, bulldog, husky, poodle, persa, maineCoon, siames, bengala));
