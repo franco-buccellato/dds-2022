434147659d3dc7fd0dda157ee6637ee8088411b9
franco-buccellato <francbuccellato@frba.utn.edu.ar>
2021-05-06 22:58:50 -0300

    Test Básicos Rescatista

--- src/test/java/domain/rescatista/RescatistaTest.java
+++ src/test/java/domain/rescatista/RescatistaTest.java
@@ -11,25 +11,37 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.io.File;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class RescatistaTest {
 
     Rescatista donRescatista;
     Rescatista doniaRescatista;
+    Image fotoPerro1Encontrado;
+    List<Image> fotos1;
 
     @BeforeEach
+
     public void setup() throws IOException {
+
+        fotoPerro1Encontrado = ImageIO.read(new File("resources/images/perroEncontrado1.jpg"));
+        fotos1 = new ArrayList<>(Collections.singletonList(fotoPerro1Encontrado));
+
         donRescatista = new Rescatista("Don", "Rescatista", DNI,
                 "99999999", LocalDate.of(1995, 5, 5),
-                new MascotaEncontrada(null ,"Canino macho, color negro, raza caniche",
+                new MascotaEncontrada(fotos1 ,"Canino macho, color negro, raza caniche",
                         "Estacion de Tren 'El Rescate'", LocalDate.of(2021, 4, 4)),
                 new Contacto("don", "Rescatista", "1234", "donrescatista@rescatista.com", "El Rescatista 123"));
         doniaRescatista = new Rescatista("Donia", "Rescatista", DNI,
                 "88888888",  LocalDate.of(1995, 4, 4),
-                new MascotaEncontrada(null ,"Canino hembra, color blanco, raza caniche",
+                new MascotaEncontrada(fotos1,"Canino hembra, color blanco, raza caniche",
                 "Estacion de Tren 'El Rescatado'", LocalDate.of(2021, 5, 4)),
                 new Contacto("don", "Rescatista", "1234", "doniarescatista@rescatista.com", "La Rescatista 321"));
     }
@@ -42,7 +54,7 @@ public class RescatistaTest {
         NullPointerException exception = assertThrows(NullPointerException.class, () -> {
             new Rescatista("Don", null, DNI,
                     "99999999", LocalDate.of(1995, 5, 5),
-                    new MascotaEncontrada(null ,"Canino macho, color negro, raza caniche",
+                    new MascotaEncontrada(fotos1 ,"Canino macho, color negro, raza caniche",
                             "Estacion de Tren 'El Rescate'", LocalDate.of(2021, 4, 4)),
                     new Contacto("don", "Rescatista", "1234", "donrescatista@rescatista.com", "El Rescatista 123"));
         });
