e9bdc1c076cb6be04900f2e32a4131e764ef44d7
julianpebe <julianperezbadon@hotmail.com>
2021-06-03 17:30:52 -0300

    Arregla test de publicacion

--- src/main/java/domain/Duenio.java
+++ src/main/java/domain/Duenio.java
@@ -61,6 +61,10 @@ public class Duenio {
   }
 
   public Contacto contactoTitular() {
-    return this.contactos.stream().filter(contacto -> contacto.getVinculo().equals(Vinculo.TITULAR)).findFirst().get();
+    return this.getContactos()
+               .stream()
+               .filter(contacto -> contacto.getVinculo().equals(Vinculo.TITULAR))
+               .findFirst()
+               .get();
   }
 }
